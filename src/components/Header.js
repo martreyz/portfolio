facb30cad36efd4842b96e467ff7c8cc3bfd61fd
martreyz <202008Mar*>
2020-12-18 11:37:59 +0100

    Add links to footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,31 +3,33 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="header__menu">
-      <button className="header__menu-lang">En</button>
-      <ul className="header__menu-pages">
-        <li className="header__menu-home">
-          <Link to="/" className="header__menu-home">
-            Inicio
-          </Link>
-        </li>
-        <li className="header__menu-projects">
-          <Link to="/projects" className="header__menu-projects">
-            Proyectos
-          </Link>
-        </li>
-        <li className="header__menu-about">
-          <Link to="/about" className="header__menu-about">
-            Sobre mí
-          </Link>
-        </li>
-        <li className="header__menu-contact">
-          <Link to="/contact" className="header__menu-contact">
-            Contacto
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="header">
+      <nav className="header__menu">
+        <button className="header__menu-lang">En</button>
+        <ul className="header__menu-pages">
+          <li className="header__menu-home">
+            <Link to="/" className="header__menu-home">
+              Inicio
+            </Link>
+          </li>
+          <li className="header__menu-projects">
+            <Link to="/projects" className="header__menu-projects">
+              Proyectos
+            </Link>
+          </li>
+          <li className="header__menu-about">
+            <Link to="/about" className="header__menu-about">
+              Sobre mí
+            </Link>
+          </li>
+          <li className="header__menu-contact">
+            <Link to="/contact" className="header__menu-contact">
+              Contacto
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
